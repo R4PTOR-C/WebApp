3ce787bfa9d995623a2a8b16d7acd80f71481c07
rafael <you@example.com>
2023-05-12 10:47:42 -0300

    Mudanças de layout e rotas atualizadas

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   get "show", to: "produtos#show"
   get "produtos/busca", to: "produtos#busca", as: :busca_produto
-  get "home", to: "produtos#home"
+  get "index", to: "produtos#index"
   delete "produtos/:id", to: "produtos#destroy", as: :delete_produto
   post "produtos", to: "produtos#create"
   get "new", to: "produtos#new"
-  root to: "produtos#index"
+  root to: "produtos#home"
 end
